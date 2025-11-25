23ea010775eab1ac5eacb8a969d9ac0724566b2a
Milena <mimoliza@gmail.com>
2025-11-25 20:25:42 +0200

    feat: implemented retry mechanism, so if lunch-svc is not responding, the application will retry for 12 seconds before showing the error message

--- src/main/java/app/SchoolLunchApplication.java
+++ src/main/java/app/SchoolLunchApplication.java
@@ -3,10 +3,12 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
+@EnableRetry
 @SpringBootApplication
 @EnableFeignClients(basePackages = "app.lunch")
 public class SchoolLunchApplication {
